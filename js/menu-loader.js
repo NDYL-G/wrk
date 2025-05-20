e4fad66d7fafde562070776d1d6c5ab2f4716508
NDYL-G <gareth@ndyl.uk>
2025-05-20 21:42:25 +0100

    Update menu-loader.js

--- js/menu-loader.js
+++ js/menu-loader.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const nav = document.getElementById('main-menu');
   nav.innerHTML = `
-    <a class="nav-logo" href="/">John Smith</a>
+    <a class="nav-logo" href="/">Wade Roberts-Knowles</a>
     <div class="nav-links">
       <a class="nav-button" href="/">Home</a>
       <a class="nav-button" href="/html/about.html">About</a>
